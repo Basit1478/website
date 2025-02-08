6ee9fd0dc09673a455956d68a0af4b535a09ee61
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-02-08 19:08:30 +0500

    Update page.tsx

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,6 +7,8 @@ import { Loader } from 'lucide-react';
 import { addToCart } from '@/app/redux/cartSlice';
 import { client } from '@/sanity/lib/client';
 import Brand from '@/components/brand';
+import NavbarClient from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 type Product = {
   _id: number;
@@ -59,8 +61,8 @@ async function getData(slug: string) {
   return { product, relatedProducts };
 }
 
-const ProductListing = ({ params }: { params: { slug: string } }) => {
-  const [currentSlug, setCurrentSlug] = useState(params.slug);
+const ProductListing = ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = React.use(params); // Unwrap the Promise to access the actual slug
   const [product, setProduct] = useState<Product | null>(null);
   const [relatedProducts, setRelatedProducts] = useState<Product[]>([]);
   const [popupMessage, setPopupMessage] = useState<string | null>(null);
@@ -76,8 +78,10 @@ const ProductListing = ({ params }: { params: { slug: string } }) => {
   }, []);
 
   useEffect(() => {
-    fetchData(currentSlug);
-  }, [currentSlug, fetchData]);
+    if (slug) {
+      fetchData(slug);
+    }
+  }, [slug, fetchData]);
 
   const handleAddToCart = () => {
     if (!product) return;
@@ -109,104 +113,111 @@ const ProductListing = ({ params }: { params: { slug: string } }) => {
     </div>;
   }
 
+  function setCurrentSlug(slug: string): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
-    <section className="px-6 md:px-12 py-10 bg-gradient-to-b from-gray-50 to-white">
-      <div className="flex flex-col md:flex-row gap-8 items-center bg-white shadow-lg p-8 rounded-lg transform transition-transform ">
-        <div className="w-full md:w-1/2">
-          <Image
-            src={product.imageUrl}
-            width={600}
-            height={600}
-            alt={product.name}
-            className="w-full h-[540px] sm:h-[300px] md:h-[600px] object-cover rounded-lg transition-all ease-in-out transform hover:scale-105"
-            priority
-          />
-
-        </div>
-        <div className="w-full md:w-1/2 space-y-4">
-          <h1 className="text-4xl font-semibold text-gray-900">{product.name}</h1>
-          <p className="text-2xl text-gray-700">${product.price}</p>
-          <p className="text-gray-600 leading-relaxed">{product.description}</p>
-
-          {product.features && product.features.length > 0 && (
-            <div className="mt-6 p-6 border border-gray-300 rounded-md shadow-md">
-              <h3 className="text-xl font-semibold mb-4">Key Features</h3>
-              <ul className="list-disc list-inside text-gray-700">
-                {product.features.map((feature, index) => (
-                  <li key={index}>{feature}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          <div className="p-6 border rounded-md shadow-md">
-            <h3 className="text-xl font-semibold mb-4">Product Dimensions</h3>
-            <div className="text-gray-700 space-y-2">
-              <div className="flex items-center gap-2">
-                <span className="text-gray-500">Width:</span>
-                <span>{product.dimensions.width} </span>
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-gray-500">Height:</span>
-                <span>{product.dimensions.height} </span>
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-gray-500">Depth:</span>
-                <span>{product.dimensions.depth} </span>
-              </div>
-            </div>
+    <div>
+      <NavbarClient/>
+      <section className="px-6 md:px-12 py-10 bg-gradient-to-b from-gray-50 to-white">
+        <div className="flex flex-col md:flex-row gap-8 items-center bg-white shadow-lg p-8 rounded-lg transform transition-transform ">
+          <div className="w-full md:w-1/2">
+            <Image
+              src={product.imageUrl}
+              width={600}
+              height={600}
+              alt={product.name}
+              className="w-full h-[540px] sm:h-[300px] md:h-[600px] object-cover rounded-lg transition-all ease-in-out transform hover:scale-105"
+              priority
+            />
           </div>
+          <div className="w-full md:w-1/2 space-y-4">
+            <h1 className="text-4xl font-semibold text-gray-900">{product.name}</h1>
+            <p className="text-2xl text-gray-700">${product.price}</p>
+            <p className="text-gray-600 leading-relaxed">{product.description}</p>
 
-          <div className="flex flex-col sm:flex-row gap-4 mt-8">
-            <button
-              className="px-8 py-4 bg-gradient-to-r from-[#6A4C93] to-[#2A254B] text-white rounded-lg transform transition-transform hover:scale-105"
-              onClick={handleAddToCart}
-            >
-              Add to Cart
-            </button>
-            <button
-              className="px-8 py-4 bg-red-600 text-white rounded-lg transform transition-transform hover:scale-105"
-              onClick={handleAddToWishlist}
-            >
-              Wishlist ❤️
-            </button>
-          </div>
-        </div>
-      </div>
+            {product.features && product.features.length > 0 && (
+              <div className="mt-6 p-6 border border-gray-300 rounded-md shadow-md">
+                <h3 className="text-xl font-semibold mb-4">Key Features</h3>
+                <ul className="list-disc list-inside text-gray-700">
+                  {product.features.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
 
-      {popupMessage && (
-        <div className="fixed bottom-6 right-6 bg-[#2A254B] text-white px-6 py-3 rounded-lg shadow-lg animate-bounce">
-          <p>{popupMessage}</p>
-        </div>
-      )}
+            <div className="p-6 border rounded-md shadow-md">
+              <h3 className="text-xl font-semibold mb-4">Product Dimensions</h3>
+              <div className="text-gray-700 space-y-2">
+                <div className="flex items-center gap-2">
+                  <span className="text-gray-500">Width:</span>
+                  <span>{product.dimensions.width} </span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="text-gray-500">Height:</span>
+                  <span>{product.dimensions.height} </span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="text-gray-500">Depth:</span>
+                  <span>{product.dimensions.depth} </span>
+                </div>
+              </div>
+            </div>
 
-      {relatedProducts.length > 0 && (
-        <section className="mt-12">
-          <h2 className="text-3xl font-semibold mb-6 text-gray-900">You might also like</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-            {relatedProducts.map((item) => (
-              <div
-                key={item._id}
-                className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-all cursor-pointer transform hover:scale-105"
-                onClick={() => setCurrentSlug(item.slug)}
+            <div className="flex flex-col sm:flex-row gap-4 mt-8">
+              <button
+                className="px-8 py-4 bg-gradient-to-r from-[#6A4C93] to-[#2A254B] text-white rounded-lg transform transition-transform hover:scale-105"
+                onClick={handleAddToCart}
               >
-                <Image
-                  src={item.imageUrl}
-                  width={200}
-                  height={200}
-                  alt={item.name}
-                  className="w-full h-[200px] object-cover rounded-md"
-                  loading="lazy"
-                />
-                <h3 className="mt-4 text-lg font-medium text-gray-800">{item.name}</h3>
-                <p className="text-sm text-gray-600">${item.price}</p>
-              </div>
-            ))}
+                Add to Cart
+              </button>
+              <button
+                className="px-8 py-4 bg-red-600 text-white rounded-lg transform transition-transform hover:scale-105"
+                onClick={handleAddToWishlist}
+              >
+                Wishlist ❤️
+              </button>
+            </div>
           </div>
-        </section>
-      )}
-      <Brand />
-    </section>
+        </div>
+
+        {popupMessage && (
+          <div className="fixed bottom-6 right-6 bg-[#2A254B] text-white px-6 py-3 rounded-lg shadow-lg animate-bounce">
+            <p>{popupMessage}</p>
+          </div>
+        )}
+
+        {relatedProducts.length > 0 && (
+          <section className="mt-12">
+            <h2 className="text-3xl font-semibold mb-6 text-gray-900">You might also like</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+              {relatedProducts.map((item) => (
+                <div
+                  key={item._id}
+                  className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-all cursor-pointer transform hover:scale-105"
+                  onClick={() => setCurrentSlug(item.slug)}
+                >
+                  <Image
+                    src={item.imageUrl}
+                    width={200}
+                    height={200}
+                    alt={item.name}
+                    className="w-full h-[200px] object-cover rounded-md"
+                    loading="lazy"
+                  />
+                  <h3 className="mt-4 text-lg font-medium text-gray-800">{item.name}</h3>
+                  <p className="text-sm text-gray-600">${item.price}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+        )}
+        <Brand />
+      </section>
+      <Footer />
+    </div>
   );
 };
 
